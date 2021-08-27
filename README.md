152a39f9e18a72c0627912b7fbf2bc4cb74a6cbe
Cizek Lukas <lukas.cizek@monster.com>
2021-08-27 19:05:04 +0200

    Initialize project

---
+++ README.md
@@ -0,0 +1,18 @@
+# Ticker alert
+
+Get your investment under control!
+
+## To run the project for development
+
+```js
+yarn
+yarn dev
+```
+
+## To run the project for production
+
+```js
+yarn --frozen-lockfile // --production
+yarn build
+yarn start
+```
---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom';
+import { StrictMode } from 'react';
+import './styles/globalStyles.css';
+
+import App from './App';
+
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById('root'),
+);
